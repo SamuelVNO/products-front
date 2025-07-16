52f220b23dbb2f0b0de99ccc726a08e41009c537
Samuel Vitor <samvitor53@gmail.com>
2025-07-16 15:54:33 -0300

    feat/enviroments

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, catchError, throwError, map } from 'rxjs';
 import { Product, ProductListResponse, ProductFilters } from '../models/product.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:3000/products'; // Corrigido para alinhar com a API
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://products-api-production-23d8.up.railway.app/products'
+}; 
