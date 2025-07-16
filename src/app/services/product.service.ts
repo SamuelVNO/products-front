2a1c1927fc0f8ae78ba326aea1bab0dbce0951fa
Samuel Vitor <samvitor53@gmail.com>
2025-07-16 15:55:53 -0300

    fix/error

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, catchError, throwError, map } from 'rxjs';
 import { Product, ProductListResponse, ProductFilters } from '../models/product.model';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
